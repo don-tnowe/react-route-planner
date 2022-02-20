bf4f4bff967fd01f3ef3744d6afb64599ee6cd80
don-tnowe <67479453+don-tnowe@users.noreply.github.com>
2022-02-21 01:39:23 +0200

    More PointDistances cleanup

--- src/components/map/PointDistances.js
+++ src/components/map/PointDistances.js
@@ -3,61 +3,48 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarker, faFlag } from '@fortawesome/free-solid-svg-icons';
 import { calculateDistance, calculatePathLength } from './DistanceCalculation.js';
 
-const dataGetters = [
-  (idx, pts) => (
-    // First
-    idx > 1 ? {
-      point: pts[0],
-      distance: calculatePathLength(pts, 0, idx),
-      icon: faFlag,
-    } : {}
-  ),
-  (idx, pts) => (
-    // Previous
-    idx > 0 ? {
-      point: pts[idx - 1],
-      distance: calculateDistance(pts[idx - 1], pts[idx]),
-      icon: faMapMarker,
-    } : {}
-  ),
-  (idx, pts) => ( 
-    // Next
-    idx < pts.length - 1 ? {
-      point: pts[idx + 1],
-      distance: calculateDistance(pts[idx], pts[idx + 1]),
-      icon: faMapMarker,
-    } : {}
-  ),
-  (idx, pts) => (
-    // Last
-    idx < pts.length - 2 ? {
-      point: pts[pts.length - 1],
-      distance: calculatePathLength(pts, idx, pts.length - 1),
-      icon: faFlag,
-    } : {}
-  ),
+const getDisplayedData = (idx, pts) => [
+  // First
+  idx > 1 ? {
+    point: pts[0],
+    distance: calculatePathLength(pts, 0, idx),
+    icon: faFlag,
+  } : {},
+  // Previous
+  idx > 0 ? {
+    point: pts[idx - 1],
+    distance: calculateDistance(pts[idx - 1], pts[idx]),
+    icon: faMapMarker,
+  } : {},
+  // Next
+  idx < pts.length - 1 ? {
+    point: pts[idx + 1],
+    distance: calculateDistance(pts[idx], pts[idx + 1]),
+    icon: faMapMarker,
+  } : {},
+  // Last
+  idx < pts.length - 2 ? {
+    point: pts[pts.length - 1],
+    distance: calculatePathLength(pts, idx, pts.length - 1),
+    icon: faFlag,
+  } : {},
 ]
 
 export const PointDistances = ({ idx, pts }) => (
   <div className='map-point-dists'>
     {
-      dataGetters.map((x, i) => {
-        console.log(x);
-        console.log(x(idx, pts));
-        const { point, distance, icon } = x(idx, pts);
-        return (
-          point
-            ?
+      getDisplayedData(idx, pts).map(
+        ({ point, distance, icon }, i) => (
+          point ? (
             <div>
               <div className='info-icon' key={i}>
                 <FontAwesomeIcon icon={icon} color={point.color} />
               </div>
               {point.name + ': ' + Math.ceil(distance) + 'm'}
             </div>
-            :
-            null
-        );
-      })
+          ) : null
+        )
+      )
     }
   </div>
 )
